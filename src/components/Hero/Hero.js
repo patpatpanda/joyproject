42e78c8d155b1cddd6fcdfef63819c322d9a9833
patpatpanda <nils-emil1337@hotmail.se>
2024-10-30 13:48:12 +0100

    a

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,21 +8,32 @@ export default function Hero() {
   // Referenser till de två videoklippen
   const video1Ref = useRef(null);
   const video2Ref = useRef(null);
-  const [showSecondVideo, setShowSecondVideo] = useState(false);
+  const [showFirstVideo, setShowFirstVideo] = useState(true);
 
   useEffect(() => {
-    // När den första videon är slut, starta den andra och dölj den första
-    const handleVideoEnd = () => {
-      setShowSecondVideo(true); // Visa den andra videon
+    // När första videon är slut, starta den andra
+    const handleFirstVideoEnd = () => {
+      setShowFirstVideo(false); // Visa den andra videon
       video2Ref.current.play(); // Starta den andra videon
     };
 
-    // Lägg till event listener på den första videon
-    const video1 = video1Ref.current;
-    video1.addEventListener('ended', handleVideoEnd);
+    // När andra videon är slut, starta om den första
+    const handleSecondVideoEnd = () => {
+      setShowFirstVideo(true); // Återgå till den första videon
+      video1Ref.current.play(); // Starta om första videon
+    };
 
+    // Lägg till event listeners
+    const video1 = video1Ref.current;
+    const video2 = video2Ref.current;
+
+    video1.addEventListener('ended', handleFirstVideoEnd);
+    video2.addEventListener('ended', handleSecondVideoEnd);
+
+    // Rensa event listeners vid nedmontering
     return () => {
-      video1.removeEventListener('ended', handleVideoEnd);
+      video1.removeEventListener('ended', handleFirstVideoEnd);
+      video2.removeEventListener('ended', handleSecondVideoEnd);
     };
   }, []);
 
@@ -33,7 +44,7 @@ export default function Hero() {
         autoPlay
         muted
         className={styles.backgroundVideo}
-        style={{ display: showSecondVideo ? 'none' : 'block' }} // Dölj efter första videon
+        style={{ display: showFirstVideo ? 'block' : 'none' }} // Visa endast när första videon är aktiv
       >
         <source src="/images/li.mp4" type="video/mp4" />
         Din webbläsare stöder inte videouppspelning.
@@ -43,7 +54,7 @@ export default function Hero() {
         ref={video2Ref}
         muted
         className={styles.backgroundVideo}
-        style={{ display: showSecondVideo ? 'block' : 'none' }} // Visa endast när andra videon är aktiv
+        style={{ display: showFirstVideo ? 'none' : 'block' }} // Visa endast när andra videon är aktiv
       >
         <source src="/images/lo.mp4" type="video/mp4" />
         Din webbläsare stöder inte videouppspelning.
