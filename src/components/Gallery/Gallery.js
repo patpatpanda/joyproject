a9e8d06d8304ac9f42c6d3329aa8408cd02529f8
patpatpanda <nils-emil1337@hotmail.se>
2024-10-31 14:25:04 +0100

    a

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,14 +1,14 @@
 // src/components/Gallery/Gallery.js
 "use client";
 
+import Link from 'next/link'; // Importera Link från Next.js
 import styles from './Gallery.module.css';
 
 export default function Gallery() {
   const destinations = [
-    { name: "Italien", image: "/images/italy.jpg" },
-    { name: "Österrike", image: "/images/österike.jpg" },
-    { name: "Norge", image: "/images/norway.jpg" },
-   
+    { name: "Italien", image: "/images/italy.jpg", path: "/destination/italy" },
+    { name: "Österrike", image: "/images/österike.jpg", path: "/destination/Austria" },
+    { name: "Norge", image: "/images/norway.jpg", path: "/destination/norway" },
   ];
 
   return (
@@ -16,12 +16,14 @@ export default function Gallery() {
       <h2 className={styles.title}>Utvalda Destinationer</h2>
       <div className={styles.grid}>
         {destinations.map((destination, index) => (
-          <div key={index} className={styles.card}>
-            <img src={destination.image} alt={destination.name} className={styles.image} />
-            <div className={styles.overlay}>
-              <h3 className={styles.destinationName}>{destination.name}</h3>
+          <Link href={destination.path} key={index}>
+            <div className={styles.card}>
+              <img src={destination.image} alt={destination.name} className={styles.image} />
+              <div className={styles.overlay}>
+                <h3 className={styles.destinationName}>{destination.name}</h3>
+              </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </section>
